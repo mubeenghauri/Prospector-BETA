2ba74a28fa1ed17e95ae367cfca239f60c77d63a
mubeenghauri <p176107@nu.edu.pk>
2020-07-13 20:40:52 +0500

    [SearchByZipCode] added zipcode to event handler

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 let startButton = document.getElementById("start");
 let stopButton  = document.getElementById("stop");
+let zipCodeInput = document.getElementById("zipCode");
 
 startButton.style.height = "50px";
 startButton.style.width = "70px";
@@ -11,7 +12,14 @@ startButton.addEventListener('click', () => {
     // Send a message to the active tab
     // alert("here !!");
 
-    chrome.runtime.sendMessage({clicked: true});
+    var zip = zipCodeInput.value;
+
+    if(parseInt(zip) != NaN) {
+        zipCodeInput.placeholder = "Scrapping in Progress .... ";
+        chrome.runtime.sendMessage({clicked: true, zipCode: zip});
+    } else {
+        zipCodeInput.placeholder = "Invalid zipcode ?";
+    }
 });
 
 stopButton.addEventListener("click", () => {
