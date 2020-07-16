6f049665b9d2936949862a45556d390456762ee7
mubeenghauri <p176107@nu.edu.pk>
2020-07-16 23:06:39 +0500

    Sending pagenumber to background script

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ let running      = document.getElementById("on-running");
 let stopped      = document.getElementById("on-stopped");
 let info         = document.getElementById("info");
 let clearInfo    = document.getElementById("clear-info");
+let pageNum      = document.getElementById("pageNum");
 
 startButton.style.height = "50px";
 startButton.style.width = "70px";
@@ -18,14 +19,12 @@ startButton.addEventListener('click', () => {
 	console.log("Start button slicked");
     // Send a message to the active tab
     // alert("here !!");
-
     var zip = zipCodeInput.value;
-
+    var page = pageNum.value.length === 0 ? "1" : pageNum.value;
     running.hidden = false; stopped.hidden = true;
-
     if(parseInt(zip) != NaN) {
         zipCodeInput.placeholder = "Scrapping in Progress .... ";
-        chrome.runtime.sendMessage({clicked: true, zipCode: zip});
+        chrome.runtime.sendMessage({clicked: true, zipCode: zip, page: page});
     } else {
         zipCodeInput.placeholder = "Invalid zipcode ?";
     }
